25f2b67f0d49e05f9c913b3f6818b8d022c6257d
Andrew <aderevyashko@renault-belarus.by>
2019-07-18 23:18:29 +0300

    add first app on c#

---
+++ hello_world/hello_world/hello_world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string userName = CallUserName();
+            Console.WriteLine($"Hello {userName}!");
+        }
+
+        static string CallUserName()
+        {
+            Console.WriteLine("Hi, write u name...");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+    }
+}
