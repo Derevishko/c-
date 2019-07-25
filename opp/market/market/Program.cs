1589df625d5c45de091d0f3604b599be3c2b1819
Andrew <aderevyashko@renault-belarus.by>
2019-07-25 21:46:31 +0300

    add market project

---
+++ opp/market/market/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using MarketItems;
+
+namespace market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item1 = new Item(4, "Beer");
+            Item item2 = new Item(2, "Milk");
+            Item item3 = new Item(1, "Salt");
+            Item item4 = new Item(34000, "Renault Arkana");
+            Item item5 = new Item(25000, "Lada Vesta Sport");
+            Item item6 = new Item(60000, "Ranault New Caleos");
+            Item item7 = new Item(18000, "Lada Granta");
+            Item item8 = new Item(20000, "Lada XRay");
+
+            Market market1 = new Market("Evroopt");
+            Market market2 = new Market("Avtoprom");
+
+            market1.addItem(item1);
+            market1.addItem(item2);
+            market1.addItem(item3);
+
+            market2.addItem(item4);
+            market2.addItem(item5);
+            market2.addItem(item6);
+            market2.addItem(item7);
+            market2.addItem(item8);
+
+            ShoppingBasket basket1 = new ShoppingBasket();
+            
+            basket1.addItem(market1.buy("Beer"));
+            basket1.addItem(market1.buy("Beer"));
+            basket1.addItem(market1.buy("Beer"));
+            basket1.addItem(market1.buy("Salt"));
+            basket1.addItem(market1.buy("sss"));
+            basket1.addItem(market1.buy("Salt"));
+
+
+            ShoppingBasket basket2 = new ShoppingBasket();
+            basket2.addItem(market1.buy("Salt"));
+            basket2.addItem(market1.buy("Salt"));
+            basket2.addItem(market1.buy("Salt"));
+            basket2.addItem(market2.buy("Renault Arkana"));
+            basket2.addItem(market2.buy("Renault Arkana"));
+            basket2.addItem(market2.buy("Renault Arkana"));
+            basket2.addItem(market2.buy("Renault Arkana"));
+
+
+            Console.WriteLine(basket1.check);
+
+            Console.WriteLine(basket2.check);
+        }
+    }
+}
---
+++ opp/market/market/models/MarketItems.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace MarketItems {
+
+    public class PriceException : Exception
+    {
+        public PriceException() : this("Not valid price") { }
+        public PriceException(string message) : base(message) { }
+    }
+
+    public class Item
+    {
+        private int price = 0;
+        public readonly string Name;
+        public int Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.price = value;
+                }
+                else
+                {
+                    throw new PriceException();
+                }
+
+            }
+        }
+
+        public Item() : this(0) { }
+        public Item(int price) : this(price, "Not name") { }
+        public Item(string name) : this(0, name) { }
+        public Item(int price, string name)
+        {
+            this.Name = name;
+            this.price = price;
+        }
+    }
+
+    public class Market
+    {
+
+        private static Item defaultItem = new Item(0, "Nothing");
+
+        public readonly string Name;
+        private Item[] items = new Item[0];
+        public string itemsList
+        {
+            get
+            {
+                string list = "";
+                if (this.items.Length == 0) return "List is empty";
+                for ( int index = 0; index < this.items.Length; index++ )
+                {
+                    list += $"{this.items[index].Name},";
+                }
+                return list;
+            }
+        }
+
+	    public Market() : this("Market") { }
+        public Market(string Name)
+        {
+            this.Name = Name;
+        }
+
+         public Item buy(string Name)
+        {
+            for (int index = 0; index < this.items.Length; index++)
+            {
+                if (this.items[index].Name == Name) return this.items[index];
+            }
+            return Market.defaultItem;
+        }
+
+        public void addItem(Item item)
+        {
+            Item[] items = new Item[this.items.Length + 1];
+            for (int index = 0; index < this.items.Length; index++)
+            {
+                items[index] = this.items[index];
+            }
+            items[this.items.Length] = item;
+            this.items = items;
+        }
+    }
+
+
+    class ShoppingBasket
+    {
+        private Item[] items = new Item[0];
+        private static string defaultCheck = "Nothing";
+        public string check
+        {
+            get
+            {
+                int total = 0, count = 0;
+                string check = "";
+                for (int index = 0; index < this.items.Length; index++)
+                {
+                    if (this.items[index].Price == 0) continue;
+                    count++;
+                    check +=  $"{this.items[index].Name} - {this.items[index].Price}\n";
+                    total += this.items[index].Price;
+                }
+                if (count == 0) return ShoppingBasket.defaultCheck;
+                check += $"Total - {total}\n------------------------------------------";
+                return check;
+            }
+        }
+
+        public void addItem(Item item)
+        {
+            Item[] items = new Item[this.items.Length + 1];
+            for (int index = 0; index < this.items.Length; index++)
+            {
+                items[index] = this.items[index];
+            }
+            items[this.items.Length] = item;
+            this.items = items;
+        }
+
+        public void removeItem(string Name)
+        {
+            if (this.items.Length == 0) return;
+            Item[] items = new Item[this.items.Length - 1];
+            int count = 0;
+            for (int index = 0; index < this.items.Length; index++)
+            {
+                if (this.items[index].Name == Name) continue;
+                items[index] = this.items[index];
+                count++;
+            }
+            if (count == this.items.Length) return;
+            this.items = items;
+        }
+
+    }
+
+}
